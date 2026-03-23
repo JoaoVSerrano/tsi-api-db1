2b07da69287604ce931cb81a8c01ba1ad4f32472
JoaoVSerrano <jvserrano735@gmail.com>
2026-03-23 19:49:04 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import tarefasRoutes from "./routes/tarefas";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use("/tarefas", tarefasRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor executando em localhost:${PORT}`);
+});
+
